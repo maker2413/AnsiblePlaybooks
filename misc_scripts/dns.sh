d27cff2e1652b477d78b3286f888e73c55c4a112
Ethan Post <maker2413@gmail.com>
2019-04-26 16:55:56 +0000

    Moved dns script into it's own role

--- misc_scripts/dns.sh
+++ misc_scripts/dns.sh
@@ -2,11 +2,10 @@
 
 ENV=ssh
 IP=$(curl ifconfig.me)
-ZONE=Z33R1SMVNSBXAL
 
 # Creates route 53 records based on env name
 
-aws route53 change-resource-record-sets --hosted-zone-id "$Zone" --change-batch '{ "Comment": "Testing creating a record set", 
+aws route53 change-resource-record-sets --hosted-zone-id Z33R1SMVNSBXAL --change-batch '{ "Comment": "Testing creating a record set", 
 "Changes": [ { "Action": "UPSERT", "ResourceRecordSet": { "Name": 
 "'"$ENV"'.ethanp.dev", "Type": "A", "TTL": 
 600, "ResourceRecords": [ { "Value": "'"$IP"'" } ] } } ] }'
